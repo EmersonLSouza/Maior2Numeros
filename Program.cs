6186f4c03024e2769d1a7fd0c059c27fc06c0a1b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 20:38:45 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int primeiro, segundo, maior;
+
+Console.Write("Digite o primeiro número..: ");
+primeiro = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número...: ");
+segundo= Convert.ToInt32(Console.ReadLine());
+
