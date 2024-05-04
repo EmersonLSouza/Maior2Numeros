ba103017b674a0f1954c15feab0fe8d4eb267b04
Emerson Lima de Souza <162760073+EmersonLSouza@users.noreply.github.com>
2024-05-03 21:56:52 -0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -6,3 +6,14 @@ primeiro = Convert.ToInt32(Console.ReadLine());
 Console.Write("Digite o segundo número...: ");
 segundo= Convert.ToInt32(Console.ReadLine());
 
+if (primeiro > segundo)
+{
+    maiorNumero = primeiro;
+}
+else
+{
+    maiorNumero = segundo;
+}
+
+Console.WriteLine($"O maior número é {maiorNumero}");
+
